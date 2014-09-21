482fe75baa2fdeb12397bfd4a9039c40f5a0f0db
Eric Nelson <eric.nelson@boundarydevices.com>
2014-09-21 16:12:36 -0700

    allow override of vendor through LIBBT_VENDORFILE
    Change-Id: I3e44e7203081a40491f4e9010a12c99fa214e43e
    Signed-off-by: Eric Nelson <eric.nelson@boundarydevices.com>

--- vnd_buildcfg.mk
+++ vnd_buildcfg.mk
@@ -1,8 +1,14 @@
 generated_sources := $(local-generated-sources-dir)
 
-SRC := $(call my-dir)/include/$(addprefix vnd_, $(addsuffix .txt,$(basename $(TARGET_DEVICE))))
+ifneq (,$(LIBBT_VENDORFILE))
+        SRC := $(LIBBT_VENDORFILE)
+else
+        SRC := $(call my-dir)/include/$(addprefix vnd_, $(addsuffix .txt,$(basename $(TARGET_DEVICE))))
+endif
+
 ifeq (,$(wildcard $(SRC)))
 # configuration file does not exist. Use default one
+$(error using generic device for BBT. This is almost certainly wrong)
 SRC := $(call my-dir)/include/vnd_generic.txt
 endif
 GEN := $(generated_sources)/vnd_buildcfg.h
